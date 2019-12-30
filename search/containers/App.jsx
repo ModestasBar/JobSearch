120e71c424403a7199d19643de1a052f54ef35c6
ModestasBar <odekst@gmail.com>
2019-12-30 12:45:30 -0600

    refactor

--- search/containers/App.jsx
+++ search/containers/App.jsx
@@ -1,33 +1,33 @@
-import React, { useState, Fragment } from 'react';
-import PostSection from './components/PostSection';
-import SearchField from './components/searchForm/SearchField';
+import React, { useState, Fragment } from "react";
+import PostSection from "./components/PostSection";
+import SearchField from "./components/searchForm/SearchField";
 
 const App = () => {
-  let [url, setUrl] = useState('');
-  let [keyw, setKeyw] = useState('');
+  let [url, setUrl] = useState("");
+  let [keyw, setKeyw] = useState("");
   let [scrapData, setScrapData] = useState([]);
 
   const handleClick = () => {
-    console.log('test');
-    fetch('http://localhost:3000/work', {
-      method: 'put',
-      headers: { 'Content-type': 'application/json' },
-      body: JSON.stringify({ url, keyw }),
+    console.log("test");
+    fetch("http://localhost:3000/work", {
+      method: "put",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify({ url, keyw })
     })
-      .then((response) => response.json())
-      .then((res) => {
+      .then(response => response.json())
+      .then(res => {
         setScrapData((scrapData = res));
         console.log(res);
       })
-      .catch((err) => new Error('Ops... Something went wrong...', err));
+      .catch(err => new Error("Ops... Something went wrong...", err));
   };
 
-  const handleUrlChange = (event) => {
-    setUrl(url = event.target.value);
+  const handleUrlChange = event => {
+    setUrl((url = event.target.value));
   };
 
-  const handleKeywChange = (event) => {
-    setKeyw(keyw = event.target.value);
+  const handleKeywChange = event => {
+    setKeyw((keyw = event.target.value));
   };
 
   const handleScrapData = () => scrapData;
--- search/containers/components/Button.js
+++ search/containers/components/Button.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React from "react";
 
 export default function Button({ onClick }) {
-    return <a className="f6 link ph3 pv2 mb2 mh2 white bg-mid-gray" href="#0" onClick={onClick}>
-            Search
-        </a>
+  return (
+    <a
+      className='f6 link ph3 pv2 mb2 mh2 white bg-mid-gray'
+      href='#0'
+      onClick={onClick}
+    >
+      Search
+    </a>
+  );
 }
